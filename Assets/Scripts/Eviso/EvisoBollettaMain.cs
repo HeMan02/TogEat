b0f1eb4855fec011f536608de1624ccad446eaa4
BruceWayne <musso.luca89@gmail.com>
2018-06-08 13:50:37 +0200

    Aggiunta scroll view tasti data

--- Assets/Scripts/Eviso/EvisoBollettaMain.cs
+++ Assets/Scripts/Eviso/EvisoBollettaMain.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class EvisoBollettaMain : MonoBehaviour {
 
+	public GameObject scrollContent;
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log ("ButtonInstantiate");
+		GameObject instance = Instantiate(Resources.Load("Button", typeof(GameObject))) as GameObject;
 	}
 	
 	// Update is called once per frame
