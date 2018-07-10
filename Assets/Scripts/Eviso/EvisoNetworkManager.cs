6bdcc70e8e9efcdebd174708a055aeb13ff357da
Luca <musso.luca89@gmail.com>
2018-07-10 17:57:04 +0200

    creato script networking di test,da configurre bene

---
+++ Assets/Scripts/Eviso/EvisoNetworkManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvisoNetworkManager : MonoBehaviour {
+
+	public string ip = "192.168.1.192";
+	public int port = 25001;
+	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	void OnGui(){
+		Debug.Log("NETWORKKKKKKKKKKKKKKKKKKKKK");
+		if (Network.peerType == NetworkPeerType.Disconnected) {
+			if (GUI.Button (new Rect (100, 100, 100, 25), "Start Client")) {
+				Network.Connect (ip, port);
+			}
+			if (GUI.Button (new Rect (100, 125, 100, 25), "Start Server")) {
+				Network.InitializeServer (10, port);
+			}
+		} else {
+			if (Network.peerType == NetworkPeerType.Client) {
+				GUI.Label (new Rect (100, 100, 100, 25), "SONO IL CLIENT");
+				if (GUI.Button (new Rect (100, 125, 100, 25), "Logout")) {
+					Network.Disconnect (250);
+				}
+			}
+			if (Network.peerType == NetworkPeerType.Server) {
+				GUI.Label (new Rect (100, 100, 100, 25), "SONO IL SERVER");
+				GUI.Label (new Rect (100, 100, 125, 25), "Connection: " + Network.connections.Length );
+				if (GUI.Button (new Rect (100, 150, 100, 25), "Logout")) {
+					Network.Disconnect (250);
+				}
+			}
+		}
+	}
+}
