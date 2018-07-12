2d05144480c9d12d0d9a4f73f340f4a249ad839e
Luca <musso.luca89@gmail.com>
2018-07-12 17:52:13 +0200

    settato controllo da tastiera per creazione server e client

---
+++ Assets/NetworkManagerEviso.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class NetworkManagerEviso : NetworkManager {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.S)) {
+			StartServer ();
+		}
+		if (Input.GetKeyDown (KeyCode.C)) {
+			StartClient ();
+		}
+	}
+
+}
--- Assets/Scripts/Eviso/EvisoNetworkManager.cs
+++ Assets/Scripts/Eviso/EvisoNetworkManager.cs
@@ -7,22 +7,12 @@ public class EvisoNetworkManager : NetworkBehaviour {
 
 	public string ip = "192.168.1.192";
 	public int port = 25001;
-	// Use this for initialization
-//	void Start () {
-//		
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//		
-//	}
 
 	void OnServerInizialized(){
 		Debug.Log ("SERVERR!!");
 	}
 
 	void Start(){
-		Debug.LogError ("AAAAAAAAAAAA");
 		if (isServer) {
 			RpcServerToClient ();
 //			Debug.LogError ("SONO Server");
@@ -31,56 +21,7 @@ public class EvisoNetworkManager : NetworkBehaviour {
 //			Debug.LogError ("SONO Client");
 		}
 	}
-
-//	void OnGUI(){
-////		Debug.Log("NETWORKKKKKKKKKKKKKKKKKKKKK");
-//		if (Network.peerType == NetworkPeerType.Disconnected) {
-//			if (GUI.Button (new Rect (100, 100, 100, 25), "Start Client")) {
-//				Network.Connect (ip, port);
-//			}
-//			if (GUI.Button (new Rect (100, 125, 100, 25), "Start Server")) {
-//				Network.InitializeServer (10, port);
-//			}
-//		} else {
-//			if (Network.peerType == NetworkPeerType.Client) {
-//				GUI.Label (new Rect (100, 100, 100, 25), "CLIENT");
-//				if (GUI.Button (new Rect (100, 150, 100, 25), "Logout")) {
-//					Network.Disconnect (250);
-//				}
-//				if (GUI.Button (new Rect (100, 175, 100, 25), "testClient")) {
-//					GUI.Label (new Rect (100, 200, 100, 25), "ClientToServer");
-//					Debug.LogError ("STAMPA ClientToServer");
-////					RpcServerToClient();
-//					if (isServer) {
-//						Debug.LogError ("SONO Server");
-//					} else {
-//						Debug.LogError ("SONO Client");
-//						CmdClientToServer ();
-//					}
-//				}
-//			}
-//			if (Network.peerType == NetworkPeerType.Server) {
-//				GUI.Label (new Rect (100, 100, 100, 25), "SERVER");
-//				GUI.Label (new Rect (100, 125, 100, 25), "Connection: " + Network.connections.Length );
-//				if (GUI.Button (new Rect (100, 150, 100, 25), "Logout")) {
-//					Network.Disconnect (250);
-//				}
-//					if (GUI.Button (new Rect (100, 175, 100, 25), "testServer")) {
-//						GUI.Label (new Rect (100, 200, 100, 25), "SerToClient");
-//						Debug.LogError ("STAMPA ServerToClient");
-////					CmdClientToServer();
-//
-//						if (isServer) {
-//							Debug.LogError ("SONO Server");
-//							RpcServerToClient ();
-//						} else {
-//							Debug.LogError ("SONO Client");
-//						}
-//					}
-//				}
-//			
-//		}
-//	}
+		
 
 	[ClientRpc]
 	void RpcServerToClient(){
