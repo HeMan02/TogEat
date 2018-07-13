ccd2ff82ed27ad7dc81af94a77b3754e868dd2f1
Luca <musso.luca89@gmail.com>
2018-07-13 11:10:53 +0200

    Comunicazione tra server e client creata con singol omessaggio

--- Assets/Scripts/Eviso/EvisoNetworkManager.cs
+++ Assets/Scripts/Eviso/EvisoNetworkManager.cs
@@ -5,34 +5,57 @@ using UnityEngine.Networking;
 
 public class EvisoNetworkManager : NetworkBehaviour {
 
+	// non sono convinto per ORA di utilizzare una classe statica come instance perchè non ne vedo l'utilizzo
 	public string ip = "192.168.1.192";
 	public int port = 25001;
+	public static EvisoNetworkManager Instance;
 
 	void OnServerInizialized(){
 		Debug.Log ("SERVERR!!");
 	}
 
-	void Start(){
+	// utilizzo l'instance per generare una sola copia del server e per controllare se sono l'owner andrò a vedere se esiste l'instance
+	void Awake(){
 		if (isServer) {
-			RpcServerToClient ();
-//			Debug.LogError ("SONO Server");
+			Instance = this;
+		} 
+	}
+
+	void Start(){
+		if (EvisoNetworkManager.Instance != null) {
+			RpcServerToClient (); // nell ostart del server contatta i client , in teoria sarà vuoto un domani
 		} else {
-			CmdClientToServer ();
-//			Debug.LogError ("SONO Client");
+			CmdClientToServer (); // contatto i lserver
+			CmdPrintNum (Random.Range (0, 10)); // mando un numero al server e lui me l orestituisce uguale
+		}
+	}
+
+	void Update(){ // Test per vedeer a chi manda i messaggi
+		if (Input.GetKeyDown (KeyCode.T) && isServer) {
+			RpcServerToClient (); 
 		}
 	}
 		
-
+	// Messaggio dal Server ai clients , problema che ricevono multi messaggi!!
 	[ClientRpc]
 	void RpcServerToClient(){
-		Debug.LogError ("SONO Client");
-//		GUI.Label (new Rect (100, 200, 100, 25), "SerToClient");
+			Debug.LogError ("Server scrive al Client");
+	}
 
+	// Messaggio dal Client al server
+	[Command]
+	void CmdClientToServer(){
+		Debug.LogError ("Client scrive al Server");
 	}
 
 	[Command]
-	void CmdClientToServer(){
-		Debug.LogError ("SONO Server");
-//		GUI.Label (new Rect (100, 200, 100, 25), "ClientToServer");
+	void CmdPrintNum(int num){
+		Debug.LogError ("client dice al server  il num: " + num);
+		TargetResposeToClient (connectionToClient,num);
+	}
+
+	[TargetRpc]
+	public void TargetResposeToClient(NetworkConnection target,int num){
+		Debug.LogError ("sono i lserver e ti rispondo,ho ricevuto il tuo num : " + num);
 	}
 }
